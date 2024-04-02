fe1600f01304ccbec2db92a8822e585e88f4147f
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-04-02 18:51:12 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,18 @@
+
+function maximizeGreatness(input: number[]): number {
+    input.sort((x, y) => x - y);
+
+    let indexForLowerValues = 0;
+    let indexForGreaterValues = 0;
+    let maxPossibleGreatness = 0;
+
+    while (indexForLowerValues < input.length && indexForGreaterValues < input.length) {
+        if (input[indexForLowerValues] < input[indexForGreaterValues]) {
+            ++indexForLowerValues;
+            ++maxPossibleGreatness;
+        }
+        ++indexForGreaterValues;
+    }
+
+    return maxPossibleGreatness;
+};
